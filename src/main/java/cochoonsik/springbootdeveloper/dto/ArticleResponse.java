31d8710674ce04a9a6bd1b29d9161496951fc9e0
Baejinho4028 <a01040280249@gmail.com>
2023-09-12 16:30:16 +0900

    project init

---
+++ src/main/java/cochoonsik/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package cochoonsik.springbootdeveloper.dto;
+
+import cochoonsik.springbootdeveloper.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article){
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
