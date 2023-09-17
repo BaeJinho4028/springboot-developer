a9036502677cc2f3bfa5ecbd694c281011521505
Baejinho4028 <a01040280249@gmail.com>
2023-09-17 10:42:52 +0900

    project init

--- src/main/java/cochoonsik/springbootdeveloper/controller/UserViewController.java
+++ src/main/java/cochoonsik/springbootdeveloper/controller/UserViewController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserViewController {
 
+    @GetMapping("/")
+    public String temp() {
+        return "oauthLogin";
+    }
+
     @GetMapping("/login")
     public String login() {
         return "oauthLogin";
