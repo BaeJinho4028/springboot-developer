17e5c1662374a46002b4ebe94f07fabf2c656f99
Baejinho4028 <a01040280249@gmail.com>
2023-09-29 20:35:16 +0900

    tmp

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('Hello World', '첫글은 국룰', 'user1', NOW(), NOW())
+INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('Hello World?!?', '첫글은 국룰?!?', 'user1', NOW(), NOW())
